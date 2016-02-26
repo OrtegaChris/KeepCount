1c5f17ac536a38b51a5611cc2dda843a9d161144
Chris <ortega.chris@outlook.com>
2016-02-26 17:31:40 -0500

    Update

--- app/src/main/java/keepcount/boopboop/org/keepcount/MainActivity.java
+++ app/src/main/java/keepcount/boopboop/org/keepcount/MainActivity.java
@@ -1,13 +1,18 @@
 package keepcount.boopboop.org.keepcount;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayDeque;
@@ -18,34 +23,87 @@ import java.util.Vector;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String MESSAGE = "MainActivity";
     public int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       final Deque<Integer> lastNumber = new ArrayDeque<Integer>();
+        final TextView ct = (TextView) findViewById(R.id.count);
+        final Deque<Integer> lastNumber = new ArrayDeque<Integer>();
         // creates a stack containing the prev values of count to undo errors
 
         Button ten = (Button) findViewById(R.id.plusten);
         Button five = (Button) findViewById(R.id.plusfive);
-        Button one = (Button) findViewById(R.id.plusone);
+        Button one = (Button) findViewById(R.id.plusone); // one Button
         Button clr = (Button) findViewById(R.id.clr);
         Button undo = (Button) findViewById(R.id.undo);
 
-        ToggleButton negate = (ToggleButton) findViewById(R.id.negateButton);
-// add function to the negate button (ie switch the values added by each butt +1 --> -1)
+        Button btnNScr = (Button) findViewById(R.id.ButtonNewScreen); //New Screen
+        Button browserbtn = (Button) findViewById(R.id.BrowserButton);
+        Button asyncbtn = (Button) findViewById(R.id.asyncButton);
+        // TextView tv = (TextView) findViewById(R.id.tvMain);
 
-        
-        final TextView ct = (TextView) findViewById(R.id.count);
+
+        final ToggleButton negate = (ToggleButton) findViewById(R.id.negateButton);
+        // add function to the negate button (ie switch the values added by each butt +1 --> -1)
+
+        negate.setChecked(false);
+//=============Class code=======================================================================================
+        btnNScr.setOnClickListener(new View.OnClickListener() {  // Button for class
+            @Override
+            public void onClick(View v) {
+                Log.i(MESSAGE, " It was clicked");
+                Toast t = Toast.makeText(getApplicationContext(), "Thanks for clicking", Toast.LENGTH_SHORT);
+                t.show();
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(i);
+
+
+            }
+        });
+        browserbtn.setOnClickListener(new View.OnClickListener() {  // Button for class
+            @Override
+            public void onClick(View v) {
+               // Log.i(MESSAGE, " It was clicked");
+               // Toast t = Toast.makeText(getApplicationContext(), "Thanks for clicking", Toast.LENGTH_SHORT);
+                //t.show();
+                Intent browse = new Intent(Intent.ACTION_VIEW, Uri.parse("http://pace.edu"));
+                startActivity(browse);
+
+
+            }
+        });
+        asyncbtn.setOnClickListener(new View.OnClickListener() {  // Button for class
+            @Override
+            public void onClick(View v) {
+                Log.i(MESSAGE, " Long Operation");
+                imageDownloader img= new imageDownloader(MainActivity.this);
+                img.execute("https://upload.wikimedia.org/wikipedia/commons/a/a8/Sydney_harbour_bridge_new_south_wales.jpg");
+
+
+            }
+        });
+// ======================---Keep Count code ---========================================================================
         ct.setText(Integer.toString(count));
+/*
+        negate.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                negate.toggle();
+                // change text of button
+
+
+            }
+        });
+*/
         ten.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count += 10;
-                ct.setText(Integer.toString(count));
-
-               lastNumber.push(count);
+                displayCount(count, ct);
+                lastNumber.push(count);
 
             }
 
@@ -75,12 +133,11 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 lastNumber.push(count);
                 count = 0;
+                // function to completely clear by double tapping the clear button.
 
                 Resources res = getResources();
                 String msg = res.getString(R.string.emptystack);
                 ct.setText(msg);
-
-
 
 
             }
@@ -91,14 +148,12 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
 
-                if (!lastNumber.isEmpty()){
+                if (!lastNumber.isEmpty()) {
                     count = lastNumber.pop();
 
 
-                ct.setText((Integer.toString(count)));
-            }
-                else
-                {
+                    ct.setText((Integer.toString(count)));
+                } else {
                     Resources res = getResources();
                     String msg = res.getString(R.string.emptystack);
                     ct.setText(msg);
@@ -111,7 +166,13 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-   /* @Override
+    public int displayCount(int count, TextView ct) {
+        ct.setText((Integer.toString(count)));
+        return 0;
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -128,9 +189,12 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast x = Toast.makeText(getApplicationContext(), "Settings Set", Toast.LENGTH_SHORT);
+            x.show();
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
---
+++ app/src/main/java/keepcount/boopboop/org/keepcount/MainActivity2.java
@@ -0,0 +1,14 @@
+package keepcount.boopboop.org.keepcount;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
